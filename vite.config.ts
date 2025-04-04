ca4aab3e55897ff87f30e9accb9232b6bf73fee2
Henry Mwenesi <conversationapps@gmail.com>
2025-04-04 12:43:48 +0300

    Commit all current changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      'c0f17a5e-add6-4b43-b9df-87aefac1eae9.lovableproject.com'
+    ],
   },
   plugins: [
     react(),
